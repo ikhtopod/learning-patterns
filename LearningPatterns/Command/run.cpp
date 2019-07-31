88f793eda5b51c2c8868c8a4c4d99499c84728a7
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-31 19:55:33 +0300

    added infinity test

--- LearningPatterns/Command/run.cpp
+++ LearningPatterns/Command/run.cpp
@@ -88,6 +88,41 @@ void Run() {
 			std::cout << value.Get() << std::endl;
 		}
 	}
+
+	/*{
+		using namespace std::chrono;
+
+		std::default_random_engine dre { SEED };
+		std::uniform_int_distribution uid_value { -20, 20 };
+		std::uniform_int_distribution uid_amount_operations { 0, 15 };
+		std::bernoulli_distribution bernoulli {};
+
+		HandlerOperations handler {};
+
+		Value value { uid_value(dre) };
+
+		std::cout << value.Get() << std::endl << std::endl;
+
+		while (true) {
+			int amount_operations = uid_amount_operations(dre);
+			for (int i = 0; i < amount_operations; ++i) {
+				Request request;
+				request.value = uid_value(dre);
+
+				if (bernoulli(dre)) {
+					request.operation = std::make_shared<ValueAddOperation>(&value);
+				} else {
+					request.operation = std::make_shared<ValueSubOperation>(&value);
+				}
+
+				handler.Insert(request);
+			}
+
+			while (handler.Next()) {
+				std::cout << value.Get() << std::endl;
+			}
+		}
+	}*/
 }
 
 } // Command
